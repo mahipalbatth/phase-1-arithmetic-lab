ea5fadad924a7882f598148d96767327b1823f8c
Mahipal Signh <msbatth14@gmail.com>
2022-09-01 18:28:40 -0500

    commit tree efc738b295a666e8832869c26fc02db0192cc8d2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+// const random = Math.floor(Math.random(Number.MAX_VALUE-0)+1);
+const random = Math.floor(Math.random() *Number.MAX_VALUE) +0;
+
+const num3 = 32
+const num4 = 7
+const mod = Math.floor(num3 / num4);
+
+const max = Math.max(15,16, 17, 18, 19, 20);
